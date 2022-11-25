61ec550b41564a7c13a97f3f7c42beac602bf3ab
VoVanThanh <thanhvo618@gmail.com>
2022-11-25 22:59:45 +0700

    add home ui

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,7 +1,8 @@
 const images = {
-    // logo: require("~/assets/images/logo.png"),
-    placeholder: require("~/assets/images/placeholder.jpg"),
-    imagePlaceholder: require("~/assets/images/img-placeholder.jpg"),
+  // logo: require("~/assets/images/logo.png"),
+  placeholder: require('~/assets/images/placeholder.jpg'),
+  imagePlaceholder: require('~/assets/images/img-placeholder.jpg'),
+  home: require('~/assets/images/home.png'),
 };
 
-export default images
+export default images;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,56 @@
+import { Link } from 'react-router-dom';
+import classNames from 'classnames/bind';
+
+import styles from './Home.module.scss';
+import images from '~/assets/images';
+
+const cx = classNames.bind(styles);
+
 export function Home() {
   return (
-    <>
-      <input type="text" />
-      <input type="submit" value="abc" />
-    </>
+    <div className={cx('home-container')}>
+      <div className="container">
+        <nav className="navbar">
+          <Link to="/" className={cx('home-logo', 'home-link')}>
+            HTT
+          </Link>
+          <ul className="navbar-nav mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link
+                className={cx('home-link')}
+                type="button"
+                data-bs-toggle="modal"
+                data-bs-target="#modal-login"
+              >
+                Đăng nhập
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div className={cx('d-flex', 'justify-content-center', 'home-wraper')}>
+        <img
+          alt="center"
+          src={images.home}
+          className={cx('home-wraper__image')}
+        />
+      </div>
+      <div className="container">
+        <div className={cx('home-search')}>
+          <span className={cx('home-search__title')}>
+            Hệ thống tra cứu văn bằng
+          </span>
+          <div className={cx('home-search__input')}>
+            <input className="form-control" placeholder="Họ và tên" />
+            <span>Hoặc</span>
+            <input className="form-control" placeholder="Số hiệu văn bằng" />
+            <button type="button" className={cx('home-search__btn', 'btn btn-danger')}>
+              <i class="fa-solid fa-magnifying-glass"></i>
+              Tìm kiếm
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
