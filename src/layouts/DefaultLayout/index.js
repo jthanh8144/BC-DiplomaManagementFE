7b4c594ceef6810d326bd79bb000774fc987255c
VoVanThanh <thanhvo618@gmail.com>
2022-12-10 13:37:01 +0700

    fix css

--- src/layouts/DefaultLayout/index.js
+++ src/layouts/DefaultLayout/index.js
@@ -1,12 +1,7 @@
-import { useContext } from 'react';
-
-import { AuthContext } from '~/contexts/AuthContext';
 import { Header } from '~/components/Header';
 import { ModalLogin } from '~/components/Modal';
 
 export function DefaultLayout({ children }) {
-  const { user } = useContext(AuthContext);
-
   return (
     <>
       <div className="container">
--- src/layouts/HomeLayout/index.js
+++ src/layouts/HomeLayout/index.js
@@ -1,11 +1,6 @@
-import { useContext } from 'react';
-
-import { AuthContext } from '~/contexts/AuthContext';
 import { ModalLogin } from '~/components/Modal';
 
 export function HomeLayout({ children }) {
-  const { user } = useContext(AuthContext);
-
   return (
     <>
       {children}
